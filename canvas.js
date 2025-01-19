5910881a3f1f3191a2468becfa3b935e584d9eec
yvantot <mcivannedalman@gmail.com>
2025-01-19 23:33:50 +0800

    snow

--- canvas.js
+++ canvas.js
@@ -1,100 +1,59 @@
 const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+const PI = Math.PI;
+
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
-window.onresize = () => {
+canvas.style.backgroundColor = "#ADD9F4";
+window.addEventListener("resize", () => {
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
-};
-
-const c = canvas.getContext("2d");
-const mouse = {
-	x: undefined,
-	y: undefined,
-};
-
-const colors = ["0d0630", "18314f", "384e77", "8bbeb2", "e6f9af"];
-var isClicking = false;
-window.addEventListener("mousemove", ({ x, y }) => {
-	mouse.x = x;
-	mouse.y = y;
 });
 
-window.addEventListener("mousedown", () => {
-	isClicking = true;
-});
-window.addEventListener("mouseup", () => {
-	isClicking = false;
-});
-
-var mouseBound = 100;
-function Circle(x, y, dx, dy, radius) {
-	this.x = x;
-	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
-	this.radius = radius;
-	this.minRadius = radius;
-	this.color = `#${colors[Math.floor(Math.random() * colors.length)]}`;
-
-	this.draw = function () {
+class Snow {
+	constructor(x, y, dx, dy, radius, color) {
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.spawnX = this.x;
+		this.radius = radius;
+		this.color = color;
+	}
+	draw() {
 		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.arc(this.x, this.y, this.radius, 0, PI * 2, false);
 		c.fillStyle = this.color;
 		c.fill();
-	};
-
-	this.update = function () {
-		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-			this.dx = -this.dx;
-		}
-		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-			this.dy = -this.dy;
-		}
-
+	}
+	update() {
 		this.x += this.dx;
 		this.y += this.dy;
-
-		if (isClicking) {
-			if (mouse.x - this.x < mouseBound && mouse.x - this.x > -mouseBound && mouse.y - this.y < mouseBound && mouse.y - this.y > -mouseBound) {
-				if (this.x > mouse.x) this.x -= 5;
-				if (this.y > mouse.y) this.y -= 5;
-
-				if (this.x < mouse.x) this.x += 5;
-				if (this.y < mouse.y) this.y += 5;
-			}
-		} else {
-		}
-
 		this.draw();
-	};
+
+		if (this.y > innerHeight) this.y = 0;
+		if ((this.x > innerWidth || this.x < 0) && this.y > innerHeight) this.x = this.spawnX;
+	}
 }
 
-var circles = [];
-
-for (let i = 0; i < 1000; i++) {
-	var radius = Math.random() * 8 + 3;
-	var x = Math.random() * (innerWidth - radius * 2) + radius;
-	var y = Math.random() * (innerHeight - radius * 2) + radius;
-	var dx = (Math.random() - 0.5) * 2;
-	var dy = (Math.random() - 0.5) * 2;
-
-	var circle = new Circle(x, y, dx, dy, radius, "blue");
-	circles.push(circle);
+var snows = [];
+function createSnow(count) {
+	for (let i = 0; i < count; i++) {
+		const x = Math.random() * innerWidth;
+		const dx = (Math.random() - 0.5) * 2;
+		const dy = Math.random() * 3 + 1;
+		const radius = Math.random() * 2 + 1;
+		snows.push(new Snow(x, -500, dx, dy, radius, "white"));
+	}
 }
 
 function animate() {
 	requestAnimationFrame(animate);
 	c.clearRect(0, 0, innerWidth, innerHeight);
-	if (isClicking) {
-		c.beginPath();
-		c.strokeRect(mouse.x - mouseBound, mouse.y - mouseBound, mouseBound * 2, mouseBound * 2);
-		c.strokeStyle = "black";
-		c.stroke();
-	}
-	for (var i = 0; i < circles.length; i++) {
-		circles[i].update();
+	for (let i = 0; i < snows.length; i++) {
+		snows[i].update();
 	}
 }
 
+createSnow(1000);
 animate();
